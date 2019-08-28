93120c28a6336020d8fc33f7f311fdbab34ba3d4
Jyunmau <yangyang4910709@163.com>
2019-08-28 23:57:41 +0800

    基础功能完成
    调用百度智能云接口，实现语音文件到utf-8编码的转换

---
+++ CoreSource/speechRec.py
@@ -0,0 +1,23 @@
+from aip import AipSpeech
+
+# coding=utf-8
+
+# 百度智能云账户参数配置
+APP_ID = '17134145'
+API_KEY = 'yDTW0ljcQd24ZKyaHYRTDleX'
+SECRET_KEY = 'O6de7NZmhxd6KZILjZj2oHoqITdRoHyg'
+client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+
+
+# 读取文件
+def get_file_content(file_path):
+    with open(file_path, 'rb') as fp:
+        return fp.read()
+
+
+# 识别本地文件
+def do_tts(file_path, file_format):
+    ret = client.asr(get_file_content(file_path), file_format, 16000, {
+        'dev_pid': 1537,
+    })
+    return ret['result'][0].encode('utf-8')
