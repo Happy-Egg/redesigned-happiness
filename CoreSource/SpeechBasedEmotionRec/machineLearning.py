e326c03192d2be794e082aec653a34d5596f3bbe
Jyunmau <yangyang4910709@163.com>
2019-09-06 14:17:14 +0800

    参数调整提高正确率
    最高正确率50.83%

--- CoreSource/SpeechBasedEmotionRec/machineLearning.py
+++ CoreSource/SpeechBasedEmotionRec/machineLearning.py
@@ -2,6 +2,7 @@ from sklearn import svm
 import glob
 from random import shuffle
 from CoreSource.SpeechBasedEmotionRec import speechFeature as sf
+import matplotlib.image as img
 import numpy as np
 
 EMOTION_LABEL = {'angry': '1', 'fear': '2', 'happy': '3', 'neutral': '4', 'sad': '5', 'surprise': '6'}
@@ -14,7 +15,7 @@ def all_file_path(path='../../AudioFiles/CASIA_database/', file_type='wav'):
     return read_files
 
 
-def get_data(files, feature_model='all', split_ratio=0.8):
+def get_data(files, feature_model='all', split_ratio=0.9):
     """生成特征数据集"""
     data_feature = []
     data_labels = []
@@ -39,7 +40,7 @@ def get_data(files, feature_model='all', split_ratio=0.8):
 
 def do_train(train_data, train_label, test_data, test_label):
     """SVM训练模型"""
-    clf = svm.SVC(decision_function_shape='ovo', kernel='rbf', C=19, gamma=0.0001)
+    clf = svm.SVC(decision_function_shape='ovo', kernel='rbf', C=12, gamma=0.0001)
     clf.fit(train_data, train_label)
     acc = clf.score(test_data, test_label)
     print('>>====================Train Over===================<<')
